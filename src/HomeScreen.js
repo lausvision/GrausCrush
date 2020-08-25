8d8d63f8fee8780816ded36d55c45fb89cdd3085
laura <lauramolinalopez02@gmail.com>
2020-08-25 14:08:41 +0200

    puesta a punto 2, para pasar a firebase

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -4,17 +4,14 @@ import hall from './img/a.jpg';
 
 class HomeScreen extends Component{
 
-
-   
-
     render(){
         return(
            
             <div class = "Hall">
                 
                <img class = "imageHall" src={hall} alt="castle university"></img>
-               <div class="intro">Museum of the Netherlands, in Amsterdam. <br/>Dutch art and history from the Middle Ages<br/> to the present day.</div>
-               <div class="intro2">React App made with the API of the museum.</div>
+               <div class="intro">¿Quieres acceder a un grado universitario?</div>
+               <div class="intro2">Visualiza todos los grados universitarios de Catalunya.<br/> Guarda tus grados favoritos.</div>
             </div>
 
                 
--- src/SecondScreen.js
+++ src/SecondScreen.js
@@ -7,37 +7,17 @@ import './App.css';
 class SecondScreen extends Component{
 
 
-    state={imageURL: ""};
-    state={maker: ""};
-    state={title: ""};
-    state={prodPlace: ""};
-    state={linktoweb: ""};
+    state={algo: ""};
   
     async componentWillMount(){
-        const res = await axios.get("https://www.rijksmuseum.nl/api/nl/collection?key=TlcG1N0t&toppieces=true");
-        this.setState({imageURL: res.data.artObjects[0].webImage.url});
-        this.setState({maker: res.data.artObjects[0].principalOrFirstMaker});
-        this.setState({title: res.data.artObjects[0].title});
-        this.setState({prodPlace: res.data.artObjects[0].productionPlaces[0]});
-        this.setState({linktoweb: res.data.artObjects[0].links.web});
-        console.log(res.data);
-  
-        if(this.prodPlace===undefined){
-            this.setState({prodPlace: "undefined"});
-          }
+       
       }
 
     render(){
         return(
             <div>
                 
-                <img class = "image" src={this.state.imageURL} alt="Rembrandt pic"></img>
-                <h1 class = "title">{this.state.title}</h1>
-                <div class = "author">Author: {this.state.maker}</div>
-                <div class = "place">Placement: {this.state.prodPlace}</div>
                 
-                <a class="web" href={this.state.linktoweb}>Official site 
-                </a>
 
             </div>
             
